071be1b04d6b5cd35e5ca3f0f75ce14207140a62
Alexsandro dos Santos Ramos <144482457+alexsandroata@users.noreply.github.com>
2023-10-05 20:53:26 -0300

    update

--- README.md
+++
@@ -1,8 +0,0 @@
-# XELA_Notify
-Mod criado por mim para mta SA
-__
-## Comandos do mod:
-/avisoadm
-/avisopm
-/avisohp
-/avisomec
--- config.lua
+++ config.lua
@@ -1,11 +1,23 @@
 config = {
 
+	limite = 5, -- Máximo de infobox por vez
+
+
 	anuncio_adm = {'Staff', 'Console'}, --ACL de quem pode mandar avisos de Adm
 	anuncio_pm = {'Policial'}, --ACL de quem pode mandar avisos de Policia
 	anuncio_mec = {'Mecanica'}, --ACL de quem pode mandar avisos de Mecanica
 	anuncio_hp = {'Hospital'}, --ACL de quem pode mandar avisos de Hospital
+	
 	-- anuncio_ilegal = {"Chefe_Ilegal"}, --ACL de quem pode mandar avisos do Ilegal
-	limite = 5, -- Máximo de infobox por vez
+	
+	
+	comandos = {
+		['avisoadm'] = 'aa', --Comando que vai ser usado para criar uma Notify de Admin
+		['avisopm'] = 'pm', --Comando que vai ser usado para criar uma Notify de Policia
+		['avisomec'] = 'mec', --Comando que vai ser usado para criar uma Notify de Mecanico
+		['avisohp'] = 'hp', --Comando que vai ser usado para criar uma Notify de Hospital
+	
+	},
 
 	types = {
 		['Sucesso'] = {
@@ -26,12 +38,6 @@ config = {
 			sound = 'assets/sounds/error.wav',
 		},
 
-		['Admin'] = {
-			color = {194, 16, 194},
-			image = 'assets/images/adminduty.png',
-			sound = 'assets/sounds/adminduty.mp3',
-		},
-
 		['Ajuda'] = {
 			color = {221, 166, 25},
 			image = 'assets/images/help.png',
@@ -50,6 +56,12 @@ config = {
 			sound = 'assets/sounds/info.mp3',
 		},
 
+		['Admin'] = {
+			color = {194, 16, 194},
+			image = 'assets/images/adminduty.png',
+			sound = 'assets/sounds/adminduty.mp3',
+		},
+
 		['Policia'] = {
 			color = {50, 129, 246},
 			image = 'assets/images/police.png',
@@ -61,6 +73,7 @@ config = {
 			image = 'assets/images/hp.png',
 			sound = 'assets/sounds/siren.mp3',
 		},
+
 		['Mecanica'] = {
 			color = {221, 166, 25},
 			image = 'assets/images/mec.png',
